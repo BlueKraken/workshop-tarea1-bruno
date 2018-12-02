40c2958da9eaba8999949fa0367f9e402446dac4
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-12-02 19:30:39 -0300

    extrae datos a su propio archivo

---
+++ datos.ts
@@ -0,0 +1,35 @@
+export const dataSable = {
+    tipo: 'sable',
+    tipoDanio: 'cortante',
+    danio: 10,
+    efectoAlteracion: {
+        tipoEfecto: 'sangrado',
+        duracion: 3,
+        danioPorTurno: 5,
+        prob: 0.2
+    }
+}
+
+export const dataDaga = {
+    tipo: 'daga',
+    tipoDanio: 'punzante',
+    danio: 7,
+    efectoAlteracion: {
+        tipoEfecto: 'sangrado',
+        duracion: 4,
+        danioPorTurno: 2,
+        prob: 0.4
+    }
+}
+
+export const dataMaza = {
+    tipo: 'maza',
+    tipoDanio: 'contundente',
+    danio: 14,
+    efectoAlteracion: {
+        tipoEfecto: 'aturdimiento',
+        duracion: 2,
+        danioPorTurno: 0,
+        prob: 0.08
+    }
+}
--- tarea1Bruno.ts
+++ tarea1Bruno.ts
@@ -1,3 +1,5 @@
+import { dataDaga, dataMaza, dataSable } from './datos';
+
 interface EfectoAlteracion {
     tipoEfecto: string;
     duracion: number;
@@ -19,42 +21,6 @@ class Arma{
 
     toString(): void{
         console.log('Arma: ' + this.tipo + '. Tipo de daño: ' + this.tipoDanio + '. Daño: ' + this.danio + '.\nEfecto: ' + (this.efectoAlteracion.prob * 100) + '% de probabilidades de causar ' + this.efectoAlteracion.tipoEfecto + ' durante ' + this.efectoAlteracion.duracion + ' turnos causando ' + this.efectoAlteracion.danioPorTurno + ' de daño por turno');
-    }
-}
-
-const dataSable = {
-    tipo: 'sable',
-    tipoDanio: 'cortante',
-    danio: 10,
-    efectoAlteracion: {
-        tipoEfecto: 'sangrado',
-        duracion: 3,
-        danioPorTurno: 5,
-        prob: 0.2
-    }
-}
-
-const dataDaga = {
-    tipo: 'daga',
-    tipoDanio: 'punzante',
-    danio: 7,
-    efectoAlteracion: {
-        tipoEfecto: 'sangrado',
-        duracion: 4,
-        danioPorTurno: 2,
-        prob: 0.4
-    }
-}
-
-const dataMaza = {
-    tipo: 'maza',
-    tipoDanio: 'contundente',
-    danio: 14,
-    efectoAlteracion: {
-        tipoEfecto: 'aturdimiento',
-        duracion: 2,
-        danioPorTurno: 0,
-        prob: 0.08
     }
 }
 
