77371a781d53b2d6532424f99a986354c019d4c8
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-12-03 02:39:58 -0300

    Inicializa paquete, instala y configura webpack.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ]
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+};
